5dfea136c45da2319577b8bb2c7c30e068d5d558
sheylax <46344454+sheylax@users.noreply.github.com>
2019-04-16 13:38:12 +0200

    Add files via upload
    a simple table

---
+++ jad/js.js
@@ -0,0 +1,14 @@
+var table = new Vue({
+    el: '#table',
+    data: {
+      rows: [
+        { id: 1, symbol: "C",},
+        { id: 3, symbol: "Li",},
+        { id: 4, symbol: "Be",},
+        { id: 11, symbol: "Na",},
+        { id: 12, symbol: "Mg",},
+        { id: 9, symbol: "K"}
+      ]
+    }
+  });
+  Resources
