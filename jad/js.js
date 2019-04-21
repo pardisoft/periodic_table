71306b90a2c08588ee8aed3bd7a682fde5688ef1
sheylax <blizzard00666@gmail.com>
2019-04-21 16:07:25 +0200

    ready to add middle elements

--- jad/js.js
+++ jad/js.js
@@ -1,27 +1,27 @@
 var table = new Vue({
-    el: '#table2  ',
+    el: '#table2',
     data: {
       rows: [
-        { e1: "Li", e2: "Be", e3: "B", e4: 'C',e5: "N", e6: "O", e7: "F", e8: 'Ne' },
-        { e1: "Na", e2: "MG", e3: "Ai", e4: 'Si',e5: "P", e6: "S", e7: "Cl", e8: 'Ar' },
-        { e1: "K", e2: "Ca", e3: "Ga", e4: 'Ge',e5: "As", e6: "Se", e7: "Br", e8: 'Kr' },
-        { e1: "Rb", e2: "Sr", e3: "In", e4: 'Sn',e5: "Sb", e6: "Te", e7: "I", e8: 'Xe' },
-        { e1: "Cs", e2: "Ba", e3: "Tl", e4: 'Pb',e5: "Bi", e6: "Po", e7: "At", e8: 'Rn' },
-        { e1: "Fr", e2: "Ra", e3: "uut", e4: 'Fl',e5: "Uup", e6: "Lv", e7: "uus", e8: 'Uuo' }
+        { e3: "B", e4: 'C',e5: "N", e6: "O", e7: "F", e8: 'Ne' },
+        { e3: "Ai", e4: 'Si',e5: "P", e6: "S", e7: "Cl", e8: 'Ar' },
+        { e3: "Ga", e4: 'Ge',e5: "As", e6: "Se", e7: "Br", e8: 'Kr' },
+        {e3: "In", e4: 'Sn',e5: "Sb", e6: "Te", e7: "I", e8: 'Xe' },
+        {e3: "Tl", e4: 'Pb',e5: "Bi", e6: "Po", e7: "At", e8: 'Rn' },
+        { e3: "uut", e4: 'Fl',e5: "Uup", e6: "Lv", e7: "uus", e8: 'Uuo' }
       ]
     }
     
   });
   var table = new Vue({
-    el: '#table  ',
+    el: '#table',
     data: {
       rows: [
-        { e1: "Li", e2: "Be", e3: "B", e4: 'C',e5: "N", e6: "O", e7: "F", e8: 'Ne' },
-        { e1: "Na", e2: "MG", e3: "Ai", e4: 'Si',e5: "P", e6: "S", e7: "Cl", e8: 'Ar' },
-        { e1: "K", e2: "Ca", e3: "Ga", e4: 'Ge',e5: "As", e6: "Se", e7: "Br", e8: 'Kr' },
-        { e1: "Rb", e2: "Sr", e3: "In", e4: 'Sn',e5: "Sb", e6: "Te", e7: "I", e8: 'Xe' },
-        { e1: "Cs", e2: "Ba", e3: "Tl", e4: 'Pb',e5: "Bi", e6: "Po", e7: "At", e8: 'Rn' },
-        { e1: "Fr", e2: "Ra", e3: "uut", e4: 'Fl',e5: "Uup", e6: "Lv", e7: "uus", e8: 'Uuo' }
+        { e1: "Li", e2: "Be"},
+        { e1: "Na", e2: "MG"},
+        { e1: "K", e2: "Ca"},
+        { e1: "Rb", e2: "Sr"},
+        { e1: "Cs", e2: "Ba"},
+        { e1: "Fr", e2: "Ra"}
       ]
     }
     
