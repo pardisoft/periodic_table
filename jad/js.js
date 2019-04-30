574e5d5e6405aa1b1f68c88c5536358d647087de
sheylax <blizzard00666@gmail.com>
2019-04-30 18:31:45 +0430

    correct color7

--- jad/js.js
+++ jad/js.js
@@ -2,12 +2,12 @@ var table = new Vue({
     el: '#table3',
     data: {
       rows: [
-        { e3: { title: "B", color: " rgb(0, 255, 64)" }, e4: 'C',e5: "N", e6: "O", e7: "F", e8: 'Ne' },
-        { e3: { title: "Ai", color: " rgb(251, 255, 0)" }, e4: 'Si',e5: "P", e6: "S", e7: "Cl", e8: 'Ar' },
-        { e3: { title: "Ga", color: " rgb(251, 255, 0)" }, e4: 'Ge',e5: "As", e6: "Se", e7: "Br", e8: 'Kr' },
-        {e3: { title: "In", color: " rgb(251, 255, 0)" }, e4: 'Sn',e5: "Sb", e6: "Te", e7: "I", e8: 'Xe' },
-        {e3: { title: "Tl", color: " rgb(251, 255, 0)" }, e4: 'Pb',e5: "Bi", e6: "Po", e7: "At", e8: 'Rn' },
-        { e3: { title: "Uut", color: "grey" }, e4: 'Fl',e5: "Uup", e6: "Lv", e7: "Uus", e8: 'Uuo' }
+        { e3: { title: "B", color: " rgb(0, 255, 64)" }, e4: { title: "C", color: "darkcyan" },e5: { title: "N", color: "darkcyan" }, e6: { title: "O", color: "darkcyan" }, e7: { title: "F", color: " rgb(0, 38, 255)" }, e8: { title: "Ne", color: " rgb(132, 0, 255)" } },
+        { e3: { title: "Ai", color: " rgb(251, 255, 0)" }, e4: { title: "Si", color: " rgb(0, 255, 64)" },e5: { title: "P", color: "darkcyan" }, e6: { title: "S", color: "darkcyan" }, e7: { title: "Cl", color: " rgb(0, 38, 255)" }, e8: { title: "Ar", color: " rgb(132, 0, 255)" } },
+        { e3: { title: "Ga", color: " rgb(251, 255, 0)" }, e4: { title: "Ge", color: " rgb(0, 255, 64)" },e5: { title: "As", color: " rgb(0, 255, 64)" }, e6: { title: "Se", color: "darkcyan" }, e7: { title: "Br", color: " rgb(0, 38, 255)" }, e8: { title: "Kr", color: " rgb(132, 0, 255)" } },
+        {e3: { title: "In", color: " rgb(251, 255, 0)" }, e4: { title: "Sn", color: " rgb(251, 255, 0)" },e5: { title: "Sb", color: " rgb(0, 255, 64)" }, e6: { title: "Te", color: " rgb(0, 255, 64)" }, e7: { title: "I", color: " rgb(0, 38, 255)" }, e8: { title: "Xe", color: " rgb(132, 0, 255" } },
+        {e3: { title: "Tl", color: " rgb(251, 255, 0)" }, e4: { title: "Pb", color: " rgb(251, 255, 0)" },e5: { title: "Bl", color: " rgb(251, 255, 0)" }, e6: { title: "Po", color: " rgb(251, 255, 0)" }, e7: { title: "At", color: " rgb(0, 255, 64)" }, e8: { title: "Rn", color: " rgb(132, 0, 255)" } },
+        { e3: { title: "Uut", color: "grey" }, e4: { title: "Fl", color: " rgb(251, 255, 0)" },e5: { title: "Uup", color: "grey" }, e6: { title: "Lv", color: "grey" }, e7: { title: "Uus", color: "grey" }, e8: { title: "Uuo", color: "grey" } }
       ]
     }
     
@@ -30,9 +30,9 @@ var table = new Vue({
     el: '#table2',
     data: {
       rows: [
-        { e9: { title: "Sc", color: "#F00" }, e10: "Ti",e11: "V", e12: 'Cr',e13: "Mn", e14: "Fe", e15: { title: "Co", color: " rgb(255, 187, 0)" }, e16: { title: "Ni", color: " rgb(255, 187, 0)" }, e17: { title: "Cu", color: " rgb(255, 187, 0)" }, e18: 'Zn'},
-        { e9: { title: "Y", color: "#FF0" }, e10: "Zr",e11: "Nb", e12: 'Mo',e13: "Tc", e14: "Ru", e15: { title: "Rh", color: " rgb(255, 187, 0)" }, e16: { title: "Pd", color: " rgb(255, 187, 0)" }, e17: { title: "Ag", color: " rgb(255, 187, 0)" }, e18: 'Cd'},
-        { e9: { title: "", color: "brown" }, e10: "Hf",e11: "Ta", e12: 'W',e13: "Re", e14: "Os", e15: { title: "Ir", color: " rgb(255, 187, 0)" }, e16: { title: "Pt", color: " rgb(255, 187, 0)" }, e17: { title: "Au", color: " rgb(255, 187, 0)" }, e18: 'Hg'},
+        { e9: { title: "Sc", color: "rgb(255, 187, 0)" }, e10: "Ti",e11: "V", e12: 'Cr',e13: "Mn", e14: "Fe", e15: { title: "Co", color: " rgb(255, 187, 0)" }, e16: { title: "Ni", color: " rgb(255, 187, 0)" }, e17: { title: "Cu", color: " rgb(255, 187, 0)" }, e18: 'Zn'},
+        { e9: { title: "Y", color: "rgb(255, 187, 0)" }, e10: "Zr",e11: "Nb", e12: 'Mo',e13: "Tc", e14: "Ru", e15: { title: "Rh", color: " rgb(255, 187, 0)" }, e16: { title: "Pd", color: " rgb(255, 187, 0)" }, e17: { title: "Ag", color: " rgb(255, 187, 0)" }, e18: 'Cd'},
+        { e9: { title: "", color: "peru" }, e10: "Hf",e11: "Ta", e12: 'W',e13: "Re", e14: "Os", e15: { title: "Ir", color: " rgb(255, 187, 0)" }, e16: { title: "Pt", color: " rgb(255, 187, 0)" }, e17: { title: "Au", color: " rgb(255, 187, 0)" }, e18: 'Hg'},
         { e9: { title: "", color: "grey" }, e10: " Rf ",e11: "Db", e12: 'Sg',e13: "Bh", e14: "Hs", e15: { title: "Mt", color: "grey" }, e16: { title: "Ds", color: "grey" }, e17: { title: "Rg", color: "grey" }, e18: 'Cn'}
       ]
     }
@@ -42,8 +42,8 @@ var table = new Vue({
     el: '#table4',
     data: {
       rows: [
-        { e19: "La", e20: "Ce",e21: "Pr", e22: 'Nd',e23: "Pm", e24: "Sm", e25: "Eu", e26: 'Gd', e27: "Tb", e28: 'Dy', e29: "Ho", e30: "Er", e31: 'Tm', e32: "Yb", e33: 'Lu'},
-        { e19: "Ac", e20: "Th",e21: "Pa", e22: 'U',e23: "Np", e24: "Pu", e25: "Am", e26: 'Cm', e27: "Bk", e28: 'Cf', e29: 'Es', e30: "Fm", e31: 'Md', e32: "No", e33: 'Lr'}
+        { e19: { title: "La", color: "peru" }, e20: { title: "Ce", color: "peru" },e21: { title: "Pr", color: "peru" }, e22: { title: "Nd", color: "peru" },e23: { title: "Pm", color: "peru" }, e24: { title: "Sm", color: "peru" }, e25: { title: "Eu", color: "peru" }, e26: { title: "Gd", color: "peru" }, e27: { title: "Tb", color: "peru" }, e28: { title: "Dy", color: "peru" }, e29: { title: "Ho", color: "peru" }, e30: { title: "Er", color: "peru" }, e31: { title: "Tm", color: "peru" }, e32: { title: "Yb", color: "peru" }, e33: { title: "Lu", color: "peru" }},
+        { e19: { title: "Ac", color: "grey" }, e20: { title: "Th", color: "grey" },e21: { title: "Pa", color: "grey" }, e22: { title: "U", color: "grey" },e23: { title: "Np", color: "grey" }, e24: { title: "Pu", color: "grey" }, e25: { title: "Am", color: "grey" }, e26: { title: "Cm", color: "grey" }, e27: { title: "Bk", color: "grey" }, e28: { title: "Cf", color: "grey" }, e29: { title: "Es", color: "grey" }, e30: { title: "Fm", color: "grey" }, e31: { title: "Md", color: "grey" }, e32: { title: "No", color: "grey" }, e33: { title: "Lr", color: "grey" }}
       ]
     }
     
